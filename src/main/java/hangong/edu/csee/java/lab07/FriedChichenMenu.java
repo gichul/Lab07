65e18d1e6625c33766ba43ac480d6aaf7265ef1a
kim gi chul <21500048@handong.edu>
2018-04-04 23:41:32 +0900

    add single line comment

---
+++ src/main/java/hangong/edu/csee/java/lab07/FriedChichenMenu.java
@@ -0,0 +1,37 @@
+package hangong.edu.csee.java.lab07;
+
+public class FriedChichenMenu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		
+	}
+
+}
+
+
+class Chichen{
+	
+	public void chichen()
+	{
+	private String name;
+	private int price;
+	private int stars;
+	} 
+	
+	public void Honey_mustard`s(){
+		
+		
+	} 
+	
+	public void Spicey_mustard`s(){
+		
+		
+	} 
+	
+	
+}
